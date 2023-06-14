e971d0e8fb83eba4c2a846c5d0fe518a59f3548a
manik1241 <thmanik1241@gmail.com>
2023-06-14 11:26:00 +0600

    final push

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const instructorsCollection = client.db("milSchooldb").collection("instructors");
     const userCollection = client.db("milSchooldb").collection("users")
@@ -295,7 +295,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
